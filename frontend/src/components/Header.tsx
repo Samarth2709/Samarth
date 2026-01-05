34f25a3a312600cfed3e1fc08c4b467d5dd5506c
Samarth2709 <samarth.kumbla@gmail.com>
2026-01-05 14:40:12 -0800

    Update Header component

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -10,6 +10,7 @@ export default function Header() {
           <ul>
             <li><Link href="/">Home</Link></li>
             <li><Link href="/dashboard">Dashboard</Link></li>
+            <li><Link href="/whoop">Whoop</Link></li>
             <li><Link href="/#projects">Projects</Link></li>
             <li><Link href="/#contact">Socials</Link></li>
           </ul>
